d647cf92ff9a52cc4282dbcf06417e1327ae55c5
Timothy Fisher <timothyf@gmail.com>
2010-10-06 00:17:22 -0400

    added scripts plugin

---
+++ scripts.js
@@ -0,0 +1,23 @@
+/**
+	scripts.js
+	Scripts plugin.
+	This plugin will add RT tracking to all of the script elements contained on the page
+	Requires UTIL plugin
+*/
+
+(function(w) {
+
+var d=w.document;
+
+BOOMR = BOOMR || {};
+BOOMR.plugins = BOOMR.plugins || {};
+
+var impl = {
+	
+};
+
+BOOMR.plugins.SCRIPTS = {
+	
+};
+
+}(window));
